8f6c7e6a1b4de081689641a0dbd6feaac64ead25
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-07-29 18:09:51 -0400

    Adiciona desafio 2 de AED

--- 3/aed/lista-1/desafio_2.c
+++ 3/aed/lista-1/desafio_2.c
@@ -2,56 +2,66 @@
 #include <stdlib.h>
 #define TAMANHO_TOTAL 10
 
-typedef struct NODE{
+typedef struct NODE
+{
     int conteudo;
-    struct NODE* anterior; 
-    struct NODE* proximo; 
-}NO;
+    struct NODE *anterior;
+    struct NODE *proximo;
+} NO;
 
 int quantidade = 0;
-NO* nos = NULL;
+NO *nos = NULL;
 
-NO* criarNo(int conteudo){
-    NO* no = (NO*) malloc(sizeof(NO));
+NO *criarNo(int conteudo)
+{
+    NO *no = (NO *)malloc(sizeof(NO));
     no->anterior = NULL;
     no->proximo = NULL;
     no->conteudo = conteudo;
     return no;
 }
 
-void inserir(int conteudo){
-    NO* inicio; 
-    NO* novoNo = criarNo(conteudo);
-    if(nos == NULL){
+void inserir(int conteudo)
+{
+    NO *inicio;
+    NO *novoNo = criarNo(conteudo);
+    if (nos == NULL)
+    {
         nos = novoNo;
         return;
     }
     inicio = nos;
-    while (nos->proximo != NULL){
+    while (nos->proximo != NULL)
+    {
         nos = nos->proximo;
     }
     novoNo->anterior = nos;
     nos->proximo = novoNo;
-    
+
     quantidade++;
     nos = inicio;
 }
 
-void imprimirLista(){
-    NO* inicio = nos;
-    while (nos != NULL){
+void imprimirLista()
+{
+    NO *inicio = nos;
+    while (nos != NULL)
+    {
         printf("%d ", nos->conteudo);
         nos = nos->proximo;
     }
     nos = inicio;
 }
 
-void imprimirListaReversa(){
-    NO* inicio = nos;
-    while (nos->proximo != NULL){
+void imprimirListaReversa()
+{
+    NO *inicio = nos;
+    while (nos->proximo != NULL)
+    {
         nos = nos->proximo;
     }
-    while (nos != NULL){
+    while (nos != NULL)
+    {
         printf("%d ", nos->conteudo);
         nos = nos->anterior;
     }
@@ -69,58 +79,78 @@ Problema de Josephus
 ## Regras
 - Ao chegar no último nó, o algoritmo deve pular para o primeiro nó da lista.
 - Loop inicia no nó que é próximo ao nó que foi removido.
-*/ 
+*/
 
-NO* josephus(int rodadas){
-    NO* inicio = nos;
-    while(rodadas > 0){
-        if(nos->proximo == NULL){
+NO *josephus(int rodadas)
+{
+    NO *inicio = nos;
+    rodadas--;
+
+    while (rodadas > 0)
+    {
+        if (nos->proximo == NULL)
+        {
             nos = inicio;
-        }else{
+        }
+        else
+        {
             nos = nos->proximo;
         }
         rodadas--;
     }
+    printf("%d -> ", inicio->conteudo);
 
     int proximaRodada;
-    printf("%d ", nos->conteudo);
 
-    if(quantidade > 1){
-        if(nos->anterior == NULL){
+    if (quantidade > 1)
+    {
+        if (nos->anterior == NULL)
+        {
             nos->proximo->anterior = NULL;
-            nos = nos->proximo;
-        }else if(nos->proximo == NULL){
-            nos->anterior = NULL;
-            while(nos->anterior != NULL){
-                nos = nos->anterior;
-            }
-        }else{
-            nos->anterior->proximo = nos->proximo;
-            nos->proximo->anterior = nos->anterior;
+            proximaRodada = nos->conteudo;
             nos = nos->proximo;
         }
-
-    }else{
+        else if (nos->proximo == NULL)
+        {
+            nos->anterior->proximo = NULL;
+            proximaRodada = nos->conteudo;
+            while (nos->anterior != NULL)
+            {
+                nos = nos->anterior;
+            }
+        }
+        else
+        {
+            nos->anterior->proximo = nos->proximo;
+            nos->proximo->anterior = nos->anterior;
+            proximaRodada = nos->conteudo;
+            nos = nos->proximo;
+        }
+        quantidade--;
+    }
+    else
+    {
         return nos;
     }
     return josephus(proximaRodada);
 }
 
-int main(){
+int main()
+{
     inserir(5);
     inserir(3);
     inserir(2);
     inserir(4);
     inserir(6);
     inserir(1);
-    
+
     printf("========== LISTA COMPLETA ==========\n");
     imprimirLista();
     printf("\n");
-    
+
     printf("========== PROBLEMA DE JOSEPHUS ==========\n");
-    NO* noFinal = josephus(3);
-    // printf("%d\n", noFinal->conteudo);
+    NO *noFinal = josephus(3);
+    printf("%d\n", noFinal->conteudo);
 
     return 0;
 }
