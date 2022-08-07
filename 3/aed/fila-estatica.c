59872ea5c170d0153365e3bae798f2f7fde31503
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2022-08-07 14:32:10 -0400

    Corrige implementação estática de fila de AED1

--- 3/aed/fila-estatica.c
+++ 3/aed/fila-estatica.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 20
+#define MAX 5
 
 typedef int NODE;
 
 typedef struct QUEUE
 {
   int length;
+  int startNode;
+  int endNode;
   NODE queue[MAX];
 }QUEUE;
 
 QUEUE* createQueue(){
   QUEUE* newQueue = (QUEUE*) malloc(sizeof(QUEUE));
+  newQueue->startNode = 0;
+  newQueue->endNode = -1;
   newQueue->length = 0;
   return newQueue;
 }
 
 void push(QUEUE* queue, NODE value){
-  if(queue->length == 0){
-    queue->queue[0] = value;
-    queue->length++;
-    return;
-  }
   if(queue->length == MAX){
     printf("Lista cheia");
     return;
   }
-  queue->queue[queue->length] = value;
+  if(queue->endNode == MAX - 1){
+    queue->endNode = 0;
+  }else{
+    queue->endNode++;
+  }
+  queue->queue[queue->endNode] = value;
   queue->length++;
 }
 
@@ -35,36 +39,35 @@ void pop(QUEUE* queue){
     printf("Lista vazia");
     return;
   }
-  queue->length--;
-  int index;
-  for(index = 0; index < queue->length; index++){
-    queue->queue[index] = queue->queue[index + 1];
+  if(queue->startNode == MAX - 1){
+    queue->startNode = 0;
+  }else{
+    queue->startNode++;
   }
+  queue->length--;
 }
 
 void printAll(QUEUE* queue){
-  if(queue->length == 0){
-    printf("Lista vazia");
-    return;
-  }
   int index;
-  for(index = 0; index < queue->length; index++){
+  printf("Inicio: %d\nFim: %d\n", queue->startNode, queue->endNode);
+  for(index = 0; index < MAX; index++){
     printf("%d ", queue->queue[index]);
   }
 }
 
 int main(){
   QUEUE* queue = createQueue();
-  push(queue, 2);
-  push(queue, 20);
-  push(queue, 27);
+  push(queue, 11);
   push(queue, 22);
-  push(queue, 34);
+  push(queue, 33);
+  push(queue, 44);
+  push(queue, 55);
 
   printf("===== LISTA COMPLETA =====\n");
   printAll(queue);
   printf("\n===== LISTA COMPLETA (Após remoção)=====\n");
   pop(queue);
+  push(queue, 66);
   printAll(queue);
   return 0;
 }
